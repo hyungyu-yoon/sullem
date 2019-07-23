56b3d173d0d466c4c6c0f3304fa98801a1edb226
박신종 <qkrtlswhd@naver.com>
2019-07-23 14:08:36 +0900

    update -1

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="app">
+  <div>
     <Header></Header>
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view/> -->
   </div>
 </template>
 
