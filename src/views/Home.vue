6bc8124c5b483be9999b87507ac2c37d1cbbab9f
JO SEONG-WON <gogumassafy@gmail.com>
2019-07-25 16:35:14 +0900

    vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-  </div>
+  <HelloWorld />
 </template>
 
 <script>
-// @ is an alias to /src
+import HelloWorld from '../components/HelloWorld'
 
 export default {
-  name: 'home',
   components: {
+    HelloWorld
   }
 }
 </script>
