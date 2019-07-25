0d84810ea7db1d653390d2c484e9243622cbaa32
hyungyu <someth00@naver.com>
2019-07-25 17:59:01 +0900

    add google map place

--- src/components/Place.vue
+++ src/components/Place.vue
@@ -1,7 +1,6 @@
 <template>
     <div class='place'>
-        <h1>Hello Google~</h1>
-        <p>{{a}}</p>
+        <div id='map'></div>
     </div>
 </template>
 
@@ -10,7 +9,41 @@ export default {
   name: 'place',
   data () {
     return {
-      a: 'test'
+      map: null
+    }
+  },
+  mounted () {
+    this.initMap()
+  },
+  methods: {
+    initMap () {
+      this.map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -33.8666, lng: 151.1958 },
+        zoom: 15
+      })
+
+      // Search for Google's office in Australia.
+      var request = {
+        location: this.map.getCenter(),
+        radius: '500',
+        query: 'tourlist attraction in Sydney'
+      }
+
+      var service = new google.maps.places.PlacesService(this.map)
+      service.textSearch(request, this.callback)
+    },
+    callback (results, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          var marker = new google.maps.Marker({
+            map: this.map,
+            place: {
+              placeId: results[i].place_id,
+              location: results[i].geometry.location
+            }
+          })
+        }
+      }
     }
   }
 }
