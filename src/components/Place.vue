226ef1db32eb27240a8b0c87247282c27ac58315
hyungyu <someth00@naver.com>
2019-07-26 17:54:32 +0900

    add group list

--- src/components/Place.vue
+++ src/components/Place.vue
@@ -1,6 +1,51 @@
 <template>
     <div class='place'>
         <div id='map'></div>
+
+        <v-sheet
+    class="mx-auto"
+    elevation="8"
+
+  >
+    <v-slide-group
+      v-model="model"
+      class="pa-4"
+      prev-icon="fa-angle-left"
+      next-icon="fa-angle-right"
+      show-arrows
+    >
+      <v-slide-item
+        v-for="n in 10"
+        :key="n"
+        v-slot:default="{ active, toggle }"
+      >
+
+      <v-card
+    class="ma-6"
+    width="400"
+
+  >
+    <v-img
+      class="white--text"
+      height="200px"
+      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+    >
+      <v-card-title class="align-end fill-height">장소 이름</v-card-title>
+    </v-img>
+
+    <v-card-text>
+      <span>요약</span><br>
+      <span class="text--primary">
+        <span>Whitehaven Beach</span><br>
+        <span>Whitsunday Island, Whitsunday Islands</span>
+      </span>
+    </v-card-text>
+
+  </v-card>
+
+      </v-slide-item>
+    </v-slide-group>
+  </v-sheet>
     </div>
 </template>
 
@@ -9,7 +54,9 @@ export default {
   name: 'place',
   data () {
     return {
-      map: null
+      map: null,
+      result: null,
+      model: null
     }
   },
   mounted () {
@@ -26,7 +73,7 @@ export default {
       var request = {
         location: this.map.getCenter(),
         radius: '500',
-        query: 'tourlist attraction in Sydney'
+        query: '123+main+street'
       }
 
       var service = new google.maps.places.PlacesService(this.map)
@@ -34,7 +81,10 @@ export default {
     },
     callback (results, status) {
       if (status == google.maps.places.PlacesServiceStatus.OK) {
+        this.result = results
         for (var i = 0; i < results.length; i++) {
+          var x = results[i].photos[0].getUrl(300, 300)
+          console.log(x)
           var marker = new google.maps.Marker({
             map: this.map,
             place: {
