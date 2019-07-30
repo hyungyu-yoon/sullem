7c12f832b2b73957a55206796b2f4c85e736cfe9
JO SEONG-WON <gogumassafy@gmail.com>
2019-07-30 19:20:29 +0900

    S1P2213008-18 Make timetable

--- src/components/TimeTable.vue
+++ src/components/TimeTable.vue
@@ -1,63 +1,35 @@
 <template>
-  <table>
-    <thead>
-      <tr>
-        <th>ALL</th>
-        <th>DAY 1</th>
-        <th>DAY 2</th>
-        <th>DAY 3</th>
-      </tr>
-    </thead>
-    <tbody>
-      <template v-for="(value, index) in 24">
-        <tr :key="value">
-          <td id='time' rowspan="2" >{{ index }}:00</td>
-          <!-- dd-hh-mm -->
-          <td :id="'0-' + index + '-0'">1</td>
-          <td :id="'1-' + index + '-0'">2</td>
-          <td :id="'2-' + index + '-0'">3</td>
-        </tr>
-        <tr :key="value">
-          <td :id="'0-' + index + '-1'">1</td>
-          <td :id="'1-' + index + '-1'">2</td>
-          <td :id="'2-' + index + '-1'">3</td>
-        </tr>
-      </template>
-    </tbody>
-  </table>
+  <FullCalendar
+  defaultView="timeGridWeek"
+  :plugins="calendarPlugins"
+  :weekends="false"
+  :events="[
+    { title: 'event 1', date: '2019-04-01' },
+    { title: 'event 2', date: '2019-04-02' }
+  ]"
+  />
 </template>
 
 <script>
+import FullCalendar from '@fullcalendar/vue'
+import timeGridPlugin from '@fullcalendar/timegrid'
 export default {
+  components: {
+    FullCalendar
+  },
   data () {
     return {
-      1: {},
-      2: {},
-      3: {}
+      calendarPlugins: [ timeGridPlugin ]
     }
   }
+
 }
 </script>
 
-<style>
-  table {
-    width: 100%;
-    border: 1px solid;
-    border-collapse: collapse;
-    table-layout: fixed;
-  }
-  th, td {
-    border: 1px solid;
-  }
-  tbody {
-    display: block;
-    overflow: auto;
-    height: 400px;
-    width: 100%;
-  }
-  #time {
-    vertical-align: top;
-    width: 90px;
-    text-align: right;
-  }
+<style lang='scss'>
+
+@import '~@fullcalendar/core/main.css';
+@import '~@fullcalendar/daygrid/main.css';
+@import '~@fullcalendar/timegrid/main.css';
+
 </style>
