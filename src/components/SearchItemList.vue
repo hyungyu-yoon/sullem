2d1f9203f79f4cb43aa094d99ed991b978c6e780
hyungyu <someth00@naver.com>
2019-08-07 17:50:42 +0900

    update post search

--- src/components/SearchItemList.vue
+++ src/components/SearchItemList.vue
@@ -1,41 +1,52 @@
 <template>
   <v-layout wrap>
     <v-flex
-      v-for="i in items"
+      v-for="(result,i) in results.postList"
       :key="i"
       xs12 sm6 md4
       pa-1
     >
-      <Schedule></Schedule>
+      <schedule-card v-if="results.category == 0"></schedule-card>
+      <post-card v-if="results.category == 1"
+        :name="result.name"
+        :title="result.title"
+        :description="result.description"
+        :create-date="result.createDate"
+        :thumbnail="result.thumbnail"
+      ></post-card>
+    </v-flex>
+    <v-flex xs12>
+      <div class="text-center">
+    <v-pagination
+      v-if="results.postList.length != 0"
+      v-model="page"
+      :length="results.endPage"
+      color="teal accent-4"
+      prev-icon="fa-angle-left"
+      next-icon="fa-angle-right"
+    ></v-pagination>
+  </div>
+
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import Schedule from './Schedule.vue'
-import axios from 'axios'
+import ScheduleCard from './ScheduleCard.vue'
+import PostCard from './PostCard.vue'
+
 export default {
   components: {
-    Schedule
+    ScheduleCard,
+    PostCard
   },
   props: {
-    query: { type: String, default: ' ' },
-    category: { type: String }
+    results: { type: Object }
   },
   data () {
     return {
-      items: 5,
       page: 1
     }
-  },
-  mounted () {
-    axios.get('http://192.168.31.114:8399/post/selectPage/' + this.query + '/' + this.page)
-      .then(function (response) {
-        console.log(response.data)
-      })
-      .catch(function (error) {
-        console.log(error)
-      })
   },
   methods: {
 
