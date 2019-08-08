16126ff142eff742d400642ed2080f6979493b9e
hyungyu <someth00@naver.com>
2019-08-08 17:26:41 +0900

    modify postcard click detail

--- frontend/src/components/PostCard.vue
+++ frontend/src/components/PostCard.vue
@@ -2,6 +2,7 @@
 <v-hover v-slot:default="{ hover }">
   <v-card   :elevation="hover ? 5 : 1"
    max-height="330px"
+   @click="$router.push({name:'post', params:{no:no}})"
   >
     <v-img
       class="white--text"
@@ -9,7 +10,7 @@
       :src="thumbnail"
     >
     </v-img>
-    <v-card-title class="headline font-weight-bold">{{title}}</v-card-title>
+    <v-card-title class="headline font-weight-medium">{{title}}</v-card-title>
     <v-card-text class="subtitle-1 font-weight-bold ma-0">
       {{description}}
     </v-card-text>
@@ -20,6 +21,7 @@
 <script>
 export default {
   props: {
+    no: { type: Number },
     name: { type: String },
     title: { type: String },
     description: { type: String },
