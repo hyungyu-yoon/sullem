c1b54301e5387e2314927030f2deccbde45fd347
박신종 <qkrtlswhd@navver.com>
2019-08-08 10:25:50 +0900

    Update backend

--- backend/webmobile-project/src/main/java/com/ssafy/demo/controller/MemberController.java
+++ backend/webmobile-project/src/main/java/com/ssafy/demo/controller/MemberController.java
@@ -4,7 +4,6 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
