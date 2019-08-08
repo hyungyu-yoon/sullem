53a57a9a54e4d60d235e760d9f34eda55a46ad9c
hyungyu <someth00@naver.com>
2019-08-08 09:32:49 +0900

    delete PostPage

--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import PostPage from './views/PostPage.vue'
 import PostWriter from './views/PostWriter.vue'
 import Intro from './views/Intro.vue'
 import MapTest from './views/MapTest.vue'
@@ -32,11 +31,6 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
-      path: '/postPage',
-      name: 'postPage',
-      component: PostPage
     },
     {
       path: '/postWriter',
