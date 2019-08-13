34604a102a3fbec4bb00f472c4d6c4207db0eb63
박신종 <qkrtlswhd@navver.com>
2019-08-13 16:25:24 +0900

    update footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,16 +1,49 @@
 <template>
-    <v-footer padless height="70" color="teal accent-4">
-    <v-flex
-      text-center
-      xs12
+  <v-footer
+    dark
+    padless
+  >
+    <v-card
+      flat
+      tile
+      class="indigo teal accent-4 white--text text-center "
     >
-      {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-    </v-flex>
+      <v-card-text>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4 white--text"
+          icon
+        >
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-card-text class="white--text pt-0">
+        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</br>
+        Thank you.</br>
+        by 조성원,윤현규,박신종,윤영우,김예랑
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} — <strong>Happy Hacking</strong>
+      </v-card-text>
+    </v-card>
   </v-footer>
 </template>
 
 <script>
-export default {
-
-}
+  export default {
+    data: () => ({
+      icons: [
+        'fab fa-facebook',
+        'fab fa-twitter',
+        'fab fa-google-plus',
+        'fab fa-linkedin',
+        'fab fa-instagram',
+      ],
+    }),
+  }
 </script>
