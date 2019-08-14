91cb2cf737a6da8e6a0bb6778f39054cdb3d2237
JO SEONG-WON <gogumassafy@gmail.com>
2019-08-14 13:10:10 +0900

    Debug detail page

--- frontend/src/views/ScheduleDetailPage.vue
+++ frontend/src/views/ScheduleDetailPage.vue
@@ -22,16 +22,26 @@
             <v-card-actions>
               <v-btn @click="$router.go(-1)" color="grey lighten-3" light>목록</v-btn>
               <v-btn v-if="seq === results.seq" @click="updatePage" color="grey lighten-3" light>수정</v-btn>
-              <v-btn v-if="seq === results.seq"  @click="deleteSchedule" color="grey lighten-3" light>삭제</v-btn>
-
+              <v-btn
+                v-if="seq === results.seq"
+                @click="deleteSchedule"
+                color="grey lighten-3"
+                light
+              >삭제</v-btn>
             </v-card-actions>
           </v-card>
         </v-flex>
         <v-flex xs12 md9>
           <v-layout wrap>
             <v-tabs centered grow icons-and-text color="teal accent-4">
-              <v-tab @click="switchone" style="margin: auto;">스케쥴 <v-icon>fa-calendar-alt</v-icon></v-tab>
-              <v-tab @click="switchtwo" style="margin: auto;">경로 <v-icon>fa-map-marked-alt</v-icon></v-tab>
+              <v-tab @click="switchone" style="margin: auto;">
+                스케쥴
+                <v-icon>fa-calendar-alt</v-icon>
+              </v-tab>
+              <v-tab @click="switchtwo" style="margin: auto;">
+                경로
+                <v-icon>fa-map-marked-alt</v-icon>
+              </v-tab>
             </v-tabs>
 
             <template v-if="this.tab">
@@ -68,20 +78,18 @@
                             <v-card-title class="font-weight-bold">{{data.name}}</v-card-title>
                             <v-card-text>{{data.address}}</v-card-text>
                             <v-card-text>
-                            <v-rating
-                              v-model="data.rating"
-                              readonly
-                              size="15"
-                              color="yellow"
-                              background-color="yellow"
-                              half-increments
-
-                              empty-icon
-                              dense
-                            ></v-rating>
+                              <v-rating
+                                v-model="data.rating"
+                                readonly
+                                size="15"
+                                color="yellow"
+                                background-color="yellow"
+                                half-increments
+                                empty-icon
+                                dense
+                              ></v-rating>
                             </v-card-text>
                           </v-flex>
-
                         </v-layout>
                       </v-card>
                     </v-timeline-item>
@@ -93,22 +101,22 @@
             <!-- tab2 부분 -->
             <template v-if="!this.tab">
               <v-flex xs12>
-                <DetailMap style="width:auto" :mapEvents="events[0]" />
+                <DetailMap style="width:auto" :mapEvents="event_data" />
               </v-flex>
               <v-flex xs12>
-                <TimeTable :events="events[0]" :startDay="events[0][0].start.substring(0,10)" />
+                <TimeTable :events="event_data" :startDay="events[0][0].start.substring(0,10)" />
               </v-flex>
             </template>
 
             <v-flex xs12 mt-2>
-        <v-sheet elevation="5">
-           <v-layout>
-            <v-flex xs12 ma-5>
-           <div id="disqus_thread"></div>
-          </v-flex>
-          </v-layout>
-        </v-sheet>
-      </v-flex>
+              <v-sheet elevation="5">
+                <v-layout>
+                  <v-flex xs12 ma-5>
+                    <div id="disqus_thread"></div>
+                  </v-flex>
+                </v-layout>
+              </v-sheet>
+            </v-flex>
             <!--  -->
           </v-layout>
         </v-flex>
@@ -118,10 +126,10 @@
 </template>
 
 <script>
-import axios from 'axios'
-import DetailMap from '../components/ScheduleDetail/DetailMap.vue'
-import TimeTable from '../components/CreateSchedule/TimeTable.vue'
-import router from '@/router'
+import axios from "axios";
+import DetailMap from "../components/ScheduleDetail/DetailMap.vue";
+import TimeTable from "../components/CreateSchedule/TimeTable.vue";
+import router from "@/router";
 
 export default {
   components: {
@@ -129,105 +137,107 @@ export default {
     TimeTable
   },
   data: () => ({
-    no: '',
+    no: "",
     events: [],
-    results: '',
+    results: "",
     tab: true,
-    seq: '',
-    disqus_config: ''
+    seq: "",
+    disqus_config: "",
+    event_data: []
   }),
-  created () {
-    if (this.$session.get('user') !== undefined) {
-      this.seq = this.$session.get('user')['seq']
+  created() {
+    if (this.$session.get("user") !== undefined) {
+      this.seq = this.$session.get("user")["seq"];
     }
-    this.no = this.$route.params.no
+    this.no = this.$route.params.no;
     axios
-      .get('http://192.168.31.114:8399/schedule/selectByNo/' + this.no)
+      .get("http://192.168.31.114:8399/schedule/selectByNo/" + this.no)
       .then(response => {
-        var data = JSON.parse(response.data.events)
-        var date = ''
-        var count = -1
-        var results = []
+        var data = JSON.parse(response.data.events);
+        var date = "";
+        var count = -1;
+        var results = [];
         for (let i = 0; i < data.length; i++) {
           if (date !== data[i].start.substring(0, 10)) {
-            count++
-            results.push([])
-            date = data[i].start.substring(0, 10)
-            results[count].push(data[i])
+            count++;
+            results.push([]);
+            date = data[i].start.substring(0, 10);
+            results[count].push(data[i]);
           } else {
-            results[count].push(data[i])
+            results[count].push(data[i]);
           }
         }
-        this.events = results
+        this.events = results;
+        this.event_data = data;
 
-        this.results = response.data
-        console.log(this.results)
+        this.results = response.data;
+        console.log(this.results);
       })
-      .catch(function (error) {
-        console.log(error)
-      })
+      .catch(function(error) {
+        console.log(error);
+      });
 
-    this.disqus_config = function () {
-      this.page.url = 'http://192.168.31.129:8080/schedule/' + this.no // Replace PAGE_URL with your page's canonical URL variable
-      this.page.identifier = this.no // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-    }
+    this.disqus_config = function() {
+      this.page.url = "http://192.168.31.129:8080/schedule/" + this.no; // Replace PAGE_URL with your page's canonical URL variable
+      this.page.identifier = this.no; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
 
-    let recaptchaScript = document.createElement('script')
+    let recaptchaScript = document.createElement("script");
     recaptchaScript.setAttribute(
-      'src',
-      'https://happyhacking-1.disqus.com/embed.js'
-    )
-    document.head.appendChild(recaptchaScript)
+      "src",
+      "https://happyhacking-1.disqus.com/embed.js"
+    );
+    document.head.appendChild(recaptchaScript);
   },
   methods: {
-    getIcon (category) {
-      if (category[0] === 'lodging') {
-        return 'fa-bed'
-      } else if (category[0] === 'restaurant') {
-        return 'fa-utensils'
+    getIcon(category) {
+      if (category[0] === "lodging") {
+        return "fa-bed";
+      } else if (category[0] === "restaurant") {
+        return "fa-utensils";
       } else {
-        return 'fa-camera'
+        return "fa-camera";
       }
     },
-    getColor (category) {
-      if (category[0] === 'lodging') {
-        return 'red'
-      } else if (category[0] === 'restaurant') {
-        return 'yellow lighten-3'
+    getColor(category) {
+      if (category[0] === "lodging") {
+        return "red";
+      } else if (category[0] === "restaurant") {
+        return "yellow lighten-3";
       } else {
-        return 'teal accent-4'
+        return "teal accent-4";
       }
     },
-    switchone () {
-      this.tab = true
+    switchone() {
+      this.tab = true;
     },
 
-    switchtwo () {
-      this.tab = false
+    switchtwo() {
+      this.tab = false;
     },
-    deleteSchedule () {
-      if (confirm('일정을 정말로 삭제하시겠습니까?')) {
-        axios.delete('http://192.168.31.114:8399/schedule/delete/' + this.no)
+    deleteSchedule() {
+      if (confirm("일정을 정말로 삭제하시겠습니까?")) {
+        axios
+          .delete("http://192.168.31.114:8399/schedule/delete/" + this.no)
           .then(response => {
-            console.log(response.data)
+            console.log(response.data);
             if (response.data === 1) {
-              this.$router.go(-1)
+              this.$router.go(-1);
             } else {
-              alert('삭제에 실패했습니다.')
+              alert("삭제에 실패했습니다.");
             }
-          }
-          )
-          .catch(function (error) {
-            console.log(error)
           })
+          .catch(function(error) {
+            console.log(error);
+          });
       }
     },
-    updatePage () {
-      console.log(this.no)
-      router.push({ path: `update/${this.no}` })
+    updatePage() {
+      console.log(this.no);
+      router.push({ path: `update/${this.no}` });
     }
   }
-}
+};
 </script>
 
 <style>
